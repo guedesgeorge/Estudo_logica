e819c38a4c281d0d9b3500b830cab9991ab6ab18
´´George <george.emannuelcarvalho@gmail.com>
2024-01-20 01:46:07 -0300

    atualização logica

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let idade = prompt('Digite sua idade');
 if (idade >= 18){
-  alert('pode tirar a habilitação!');
+  alert('você tem idade para tirar a habilitação!');
 
 }else (idade <= 18)
-alert('Você não tem idade para tirar habiltação');
+alert('Você não tem idade para tirar a habiltação');
